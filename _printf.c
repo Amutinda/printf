eefe9c60e567525aa646802a45062d9d429944df
Amutinda <paulfelix829@gmail.com>
2021-07-27 19:23:49 +0000

    update

--- _printf.c
+++ _printf.c
@@ -84,6 +84,6 @@ int _printf(const char *format, ...)
 
   va_start(args, format);
 
-ret = run_printf(format, args, output);
-return (ret);
+  ret = run_printf(format, args, output);
+  return (ret);
 }
